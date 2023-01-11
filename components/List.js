05f0c7cf1f5297575f8537eb79754e790a12bbd4
Anish Maharjan <anishm@metropolia.fi>
2023-01-11 11:29:06 +0200

    ajax state task b

--- components/List.js
+++ components/List.js
@@ -1,16 +1,22 @@
 import {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
+import {baseUrl} from '../utils/variables';
 import ListItem from './ListItem';
 
 const List = () => {
-  const url =
-    'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
   const [mediaArray, setMediaArray] = useState([]);
   const loadMedia = async () => {
     try {
-      const response = await fetch(url);
+      const response = await fetch(baseUrl + 'media');
       const json = await response.json();
-      setMediaArray(json);
+      const media = await Promise.all(
+        json.map(async (file) => {
+          const fileResponse = await fetch(baseUrl + 'media/' + file.file_id);
+          return await fileResponse.json();
+        })
+      );
+
+      setMediaArray(media);
     } catch (e) {
       console.error('List, LoadMedia', e);
     }
---
+++ utils/variables.js
@@ -0,0 +1,4 @@
+const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
+const uploadsUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
+
+export {baseUrl, uploadsUrl};
