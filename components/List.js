debbb3bffeeadf206e8389fe8d4ceefdf15112de
anish0123 <anishmhn@hotmail.com>
2023-01-11 10:08:44 +0200

    ajax state- task a

--- components/List.js
+++ components/List.js
@@ -1,42 +1,22 @@
-import {
-  FlatList,
-} from 'react-native';
+import {useEffect, useState} from 'react';
+import {FlatList} from 'react-native';
 import ListItem from './ListItem';
 
-const mediaArray = [
-  {
-    key: '0',
-    title: 'Title 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/161',
-    },
-    filename: 'http://placekitten.com/2048/1920',
-  },
-  {
-    key: '1',
-    title: 'Title 2',
-    description:
-      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/164',
-    },
-    filename: 'http://placekitten.com/2041/1922',
-  },
-  {
-    key: '2',
-    title: 'Title 3',
-    description:
-      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/167',
-    },
-    filename: 'http://placekitten.com/2039/1920',
-  },
-];
-
 const List = () => {
+  const url =
+    'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+  const [mediaArray, setMediaArray] = useState([]);
+  const loadMedia = async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setMediaArray(json);
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
+  console.log('mediaArray', mediaArray);
   return (
     <FlatList
       data={mediaArray}
