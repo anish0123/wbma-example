c52f2c55d640e76d88ea968c7a0b2d87d1d21033
anish0123 <anishmhn@hotmail.com>
2023-01-12 11:18:26 +0200

    added stack navigation

--- views/Single.js
+++ views/Single.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, Text, Image} from 'react-native';
+import PropTypes from 'prop-types';
+import {uploadsUrl} from '../utils/variables';
 
-const Single = () => {
+const Single = ({route}) => {
+  console.log(route.params);
+  const {title, description, filename, time_added: t} = route.params;
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Single</Text>
+      <Text>{title}</Text>
+      <Image style={styles.image} source={{uri: uploadsUrl + filename}} />
+      <Text>{description}</Text>
+      <Text>{t}</Text>
     </SafeAreaView>
   );
 };
@@ -17,6 +24,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingTop: 40,
   },
+  image: {
+    width: 200,
+    height: 300,
+  },
 });
+
+Single.propTypes = {
+  route: PropTypes.object,
+};
 
 export default Single;
