3554d7526b05c9722fda8d56206c63a4297cf5fc
anish0123 <anishmhn@hotmail.com>
2023-01-16 09:57:01 +0200

    dummy login/logout buttons done

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import {StatusBar} from 'expo-status-bar';
+import {MainProvider} from './contexts/MainContext';
 import Navigator from './navigators/Navigator';
 
 const App = () => {
   return (
-    <>
+    <MainProvider>
       <Navigator />
-      <StatusBar style="auto" />
-    </>
+      <StatusBar style="auto" backgroundColor="rgb(0,255,0)" />
+    </MainProvider>
   );
 };
 
