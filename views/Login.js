6ee1a4e791bd9a0169a3023e19ad617673ede322
anish0123 <anishmhn@hotmail.com>
2023-01-19 10:19:00 +0200

    login form done

--- views/Login.js
+++ views/Login.js
@@ -4,27 +4,17 @@ import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useAuthentication, useUser} from '../hooks/ApiHooks';
+import LoginForm from '../components/LoginForm';
 
 const Login = ({navigation}) => {
   const {setIsLoggedIn} = useContext(MainContext);
   const {postLogin} = useAuthentication();
-  const logIn = async () => {
-    console.log('Login Button pressed');
-    const data = {username: 'anishm', password: 'anishm123'};
-    try {
-      const loginResult = await postLogin(data);
-      console.log('logIn', loginResult);
-      await AsyncStorage.setItem('userToken', loginResult.token);
-      setIsLoggedIn(true);
-    } catch (error) {
-      console.error('logIn', error);
-    }
-  };
 
   const checkToken = async () => {
     const {getUserByToken} = useUser();
     try {
       const userToken = await AsyncStorage.getItem('userToken');
+      if (userToken === null) return;
       const userData = await getUserByToken(userToken);
       console.log('checkToken', userData);
       setIsLoggedIn(true);
@@ -38,8 +28,7 @@ const Login = ({navigation}) => {
   }, []);
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
-      <Button title="Sign in!" onPress={logIn} />
+      <LoginForm />
     </View>
   );
 };
