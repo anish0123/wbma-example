29b00e57df367e0827c576f182e8058b0c89ce41
Anish Maharjan <anishm@metropolia.fi>
2023-01-30 09:31:53 +0200

    add upload view to navigator

---
+++ views/Upload.js
@@ -0,0 +1,16 @@
+import {Card} from '@rneui/themed';
+import PropTypes from 'prop-types';
+
+const Upload = ({navigation}) => {
+  return (
+    <Card>
+      <Card.Image />
+    </Card>
+  );
+};
+
+Upload.propTypes = {
+  navigation: PropTypes.object,
+};
+
+export default Upload;
