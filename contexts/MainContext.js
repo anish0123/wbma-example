de5842d004ab5cbffcd34adfc914c0b9df5570a5
Anish Maharjan <anishm@metropolia.fi>
2023-01-30 11:38:42 +0200

    wip: file upload task A 11

--- contexts/MainContext.js
+++ contexts/MainContext.js
@@ -6,9 +6,12 @@ const MainContext = React.createContext({});
 const MainProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState({});
+  const [update, setUpdate] = useState(true);
 
   return (
-    <MainContext.Provider value={{isLoggedIn, setIsLoggedIn, user, setUser}}>
+    <MainContext.Provider
+      value={{isLoggedIn, setIsLoggedIn, user, setUser, update, setUpdate}}
+    >
       {props.children}
     </MainContext.Provider>
   );
