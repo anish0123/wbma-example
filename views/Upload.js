4117f7a802612b7a94ffe5e72e8081fa2ee12d5b
Anish Maharjan <anishm@metropolia.fi>
2023-01-30 10:17:24 +0200

    image picker added

--- views/Upload.js
+++ views/Upload.js
@@ -1,11 +1,97 @@
-import {Card} from '@rneui/themed';
+import {Button, Card, Input} from '@rneui/themed';
 import PropTypes from 'prop-types';
+import {Controller, useForm} from 'react-hook-form';
+import {Keyboard, ScrollView, TouchableOpacity} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import React from 'react';
 
 const Upload = ({navigation}) => {
+  const [image, setImage] = React.useState({});
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      title: '',
+      description: '',
+    },
+  });
+
+  const upload = async (data) => {
+    // TODO: create form data and post it
+    console.log('Uploading a file', data);
+  };
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.5,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0]);
+    }
+  };
+
   return (
-    <Card>
-      <Card.Image />
-    </Card>
+    <ScrollView>
+      <TouchableOpacity onPress={() => Keyboard.dismiss()} activeOpacity={1}>
+        <Card>
+          <Card.Image
+            source={{uri: image.uri || 'https://placekitten.com/g/200/300'}}
+          />
+          <Controller
+            control={control}
+            rules={{
+              required: {
+                value: true,
+                message: 'Title is required',
+              },
+            }}
+            render={({field: {onChange, onBlur, value}}) => (
+              <Input
+                placeholder="Title"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                autoCapitalize="none"
+                errorMessage={errors.title && errors.title.message}
+              />
+            )}
+            name="title"
+          />
+          <Controller
+            control={control}
+            render={({field: {onChange, onBlur, value}}) => (
+              <Input
+                placeholder="Description"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                autoCapitalize="none"
+              />
+            )}
+            name="description"
+          />
+          <Button title="Pick an image" onPress={pickImage} />
+          <Button
+            onPress={handleSubmit(upload)}
+            radius={'sm'}
+            containerStyle={{
+              width: '100%',
+            }}
+          >
+            Upload
+          </Button>
+        </Card>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
