870b1659b4e900fcd77fa9140074738e8951d478
anish0123 <anishmhn@hotmail.com>
2023-02-01 11:45:25 +0200

    reset in upload done

--- utils/variables.js
+++ utils/variables.js
@@ -1,4 +1,5 @@
 const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
 const uploadsUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
+const appId = 'jahjksd7asjdhjkas99';
 
-export {baseUrl, uploadsUrl};
+export {baseUrl, uploadsUrl, appId};
